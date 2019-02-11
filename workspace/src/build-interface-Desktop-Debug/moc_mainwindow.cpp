24b8808f8a46c6c1217c9642f37cff0a3c343cd4
Florian <escure.f@gmail.com>
2019-02-11 18:36:21 +0100

    début interface spectateur avec actualisation de l'interface fonctionnelle

--- workspace/src/build-interface-Desktop-Debug/moc_mainwindow.cpp
+++ workspace/src/build-interface-Desktop-Debug/moc_mainwindow.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_Window_t {
-    QByteArrayData data[3];
-    char stringdata0[17];
+    QByteArrayData data[5];
+    char stringdata0[63];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -32,11 +32,14 @@ struct qt_meta_stringdata_Window_t {
 static const qt_meta_stringdata_Window_t qt_meta_stringdata_Window = {
     {
 QT_MOC_LITERAL(0, 0, 6), // "Window"
-QT_MOC_LITERAL(1, 7, 8), // "afficher"
-QT_MOC_LITERAL(2, 16, 0) // ""
+QT_MOC_LITERAL(1, 7, 22), // "InitSpectatorInterface"
+QT_MOC_LITERAL(2, 30, 0), // ""
+QT_MOC_LITERAL(3, 31, 24), // "UpdateSpectatorInterface"
+QT_MOC_LITERAL(4, 56, 6) // "update"
 
     },
-    "Window\0afficher\0"
+    "Window\0InitSpectatorInterface\0\0"
+    "UpdateSpectatorInterface\0update"
 };
 #undef QT_MOC_LITERAL
 
@@ -46,7 +49,7 @@ static const uint qt_meta_data_Window[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       1,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -54,9 +57,13 @@ static const uint qt_meta_data_Window[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   19,    2, 0x0a /* Public */,
+       1,    0,   29,    2, 0x0a /* Public */,
+       3,    0,   30,    2, 0x0a /* Public */,
+       4,    0,   31,    2, 0x0a /* Public */,
 
  // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
 
        0        // eod
@@ -68,7 +75,9 @@ void Window::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void
         Window *_t = static_cast<Window *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-        case 0: _t->afficher(); break;
+        case 0: _t->InitSpectatorInterface(); break;
+        case 1: _t->UpdateSpectatorInterface(); break;
+        case 2: _t->update(); break;
         default: ;
         }
     }
@@ -100,13 +109,13 @@ int Window::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 1)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 1;
+        _id -= 3;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 1)
+        if (_id < 3)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 1;
+        _id -= 3;
     }
     return _id;
 }
--- workspace/src/interface/mainwindow.h
+++ workspace/src/interface/mainwindow.h
@@ -2,6 +2,8 @@
 #ifndef WINDOW_H
 #define WINDOW_H
 
+#include <iostream>
+#include <string>
 #include <QWidget>
 #include <QMenu>
 #include <QMenuBar>
@@ -9,6 +11,10 @@
 #include <QLabel>
 #include <QPixmap>
 #include <QSize>
+#include <QDesktopWidget>
+#include <QMainWindow>
+#include "opencv2/core.hpp"
+#include <QGridLayout>
 
 
 class QDataWidgetMapper;
@@ -23,13 +29,27 @@ class Window : public QWidget
 {
     Q_OBJECT
 protected :
+    //QGridLayout *layout;
+    QTimer *timer;
+    int x;
+    int y;
+    bool init=true;
+    bool spectator=false;
     QAction *ouvrirAct;
     QMenu *m1;
     QString fichierchoisi;
     QSize taille;
     QPixmap pixmap;
+    QLabel * label0;
     QLabel * label1;
     QLabel * label2;
+    QLabel * label3;
+    QLabel * label4;
+    QLabel * label5;
+    QLabel * label6;
+    QLabel * label7;
+    QLabel * label8;
+    QLabel * label9;
     QImageReader *image;
     QMenuBar *mainbar;
     QStandardItemModel *model;
@@ -40,7 +60,9 @@ public:
 
 
 public slots :
-    void afficher();
+    void InitSpectatorInterface();
+    void UpdateSpectatorInterface();
+    void update();
 
 };
 #endif // WINDOW_H
