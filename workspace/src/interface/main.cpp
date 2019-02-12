48248004f1f165c739123c8ee894faada8cad976
florian <escure.f@gmail.com>
2019-02-12 13:10:21 +0100

    GUI ajout slider

--- workspace/src/interface/main.cpp
+++ workspace/src/interface/main.cpp
@@ -5,6 +5,7 @@
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
+    QCoreApplication::setAttribute(Qt::AA_UseOpenGLES);
     Window window;
     window.show();
     return app.exec();
