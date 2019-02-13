484f94c3677b72ad67e52f37e6d2dd942e486f5c
emontero <eliott.montero@etu.u-bordeaux.fr>
2019-02-13 22:10:15 +0100

    ajout test (wow)

---
+++ workspace/src/traitement/tools/test_traitement.cpp
@@ -0,0 +1,11 @@
+#include <traitement/annotation.h>
+
+using namespace traitement;
+
+int main(int argc, char ** argv) {
+  Annotation manager = new Annotation();
+
+  manager.launchAnnotation();
+
+  return 0;
+}
