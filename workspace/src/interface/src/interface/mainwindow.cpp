f7cb811fdd88fcb45cb005177cbe47fda5c65e6d
emontero <eliott.montero@etu.u-bordeaux.fr>
2019-02-25 10:27:29 +0100

    Fusion interface/traitement qui marche ! youhou !

--- workspace/src/interface/src/interface/mainwindow.cpp
+++ workspace/src/interface/src/interface/mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 
+
+
 #define CV_IMG_WIDTH 480
 #define CV_IMG_HEIGHT 640
 
@@ -65,17 +67,134 @@ MainWindow::MainWindow()
     layout->addWidget(bouton1,0,2,1,1);
     layout->addWidget(bouton2,0,4,1,1);
 
-    QTimer *timer = new QTimer(this);
-    connect(timer, SIGNAL(timeout()), this, SLOT(changeImage()));
-    timer->start(1000);
+
 
 
 
     zoneCentral->setLayout(layout);
     setCentralWidget(zoneCentral);
+
+    //Partie communication avec traitement
+
+
+    Json::Reader reader;
+    Json::Value root;
+
+
+    std::ifstream match_settings("match_settings.json");
+    if (!match_settings.good())
+      throw std::runtime_error("failde to open file match_settings.json");
+    match_settings >> root;
+
+    checkMember(root["match_setting"], "config");
+    checkMember(root["match_setting"], "field");
+    std::string conf = root["match_setting"]["config"].asString();
+    std::cout << conf << std::endl;
+
+
+
+    manager.loadConfig(conf);
+
+    std::string f = root["match_setting"]["field"].asString();
+    field.loadFile(f);
+
+    std::cout << f << std::endl;
+
+    annotation = new Annotation("annotation_settings.json");
+  // While exit was not explicitly required, run
+    now = 0;
+    dt = 30 * 1000;//[microseconds]
+    if (!manager.isLive()) {
+      now = manager.getStart();
+      //time_start = manager.getStart();
+    }
+
+    QTimer *timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(changeImage()));
+    timer->start(10);
+
+
 }
 
 void MainWindow::changeImage(){
+
+  if(!manager.isGood()){
+    while(true){
+      printf("FINI WESH");
+    }
+  }
+  else{
+    manager.update();
+    if (manager.isLive()) {
+      now = getTimeStamp();
+    } else {
+      now += dt;
+      }
+    MessageManager::Status status = manager.getStatus(now);
+    std::vector<cv::Scalar> team_colors = {cv::Scalar(255,0,255), cv::Scalar(255,255,0)};
+    std::map<uint32_t,cv::Scalar> colors_by_team;
+
+   std::map<std::string, CalibratedImage> images_by_source =
+      manager.getCalibratedImages(now);
+   for (const auto & entry : images_by_source) {
+     cv::Mat display_img = entry.second.getImg().clone();
+     if (entry.second.isFullySpecified()) {
+       const CameraMetaInformation & camera_information = entry.second.getCameraInformation();
+       field.tagLines(camera_information, &display_img, cv::Scalar(0,0,0), 2);
+       // Basic drawing of robot estimated position
+       for (const auto & robot_entry : status.robot_messages) {
+	 uint32_t team_id = robot_entry.first.team_id();
+         if (robot_entry.second.has_perception()) {
+	   const Perception & perception = robot_entry.second.perception();
+	   for (int pos_idx = 0; pos_idx < perception.self_in_field_size(); pos_idx++) {
+
+	     /* init des robots présent sur le jeu*/
+	     if (teams.find(team_id)==teams.end()){
+	       Team t1;
+	       teams[team_id]=t1;
+	     }
+	     if (!teams[team_id].IsRobot(robot_entry.first.robot_id())){
+	       teams[team_id].AddRobot(robot_entry.first.robot_id());
+	       teams[team_id].setRobotTeam(robot_entry.first.robot_id(),team_id);
+	       teams[team_id].setRobotNum(robot_entry.first.robot_id());
+
+	     }
+
+
+	     /* position du robot */
+	     const WeightedPose & weighted_pose = perception.self_in_field(pos_idx);
+	     const PositionDistribution & position = weighted_pose.pose().position();
+	     Position pos;
+	     pos.setPosition(position.x(),position.y());
+	     teams[team_id].RobotUpdate(robot_entry.first.robot_id(),pos);
+
+	     const AngleDistribution & dir = weighted_pose.pose().dir();
+	     Direction direction;
+	     direction.SetMean (dir.mean())  ;
+
+	     const PositionDistribution & ball = perception.ball_in_self();
+	     Position pos_ball;
+	     pos_ball.setPosition(ball.x(), ball.y());
+	     teams[team_id].setRobotPosBall(robot_entry.first.robot_id(), pos_ball);
+
+	     display_img =annotation->AddAnnotation(pos,direction, camera_information, teams[team_id].GetRobot(robot_entry.first.robot_id()) , display_img);
+	   }
+
+	 }
+       }
+     }
+
+     cv::cvtColor(display_img, display_img, CV_BGR2RGB);
+     this->label1->setPixmap(QPixmap::fromImage(QImage(display_img.data, display_img.cols, display_img.rows, display_img.step, QImage::Format_RGB888)));
+
+
+   }
+
+
+  }
+
+
+  /*
   tour++;
   if(tour%2==1){
     this->label1->setPixmap(QPixmap::fromImage(QImage(cvImage2->data, cvImage2->cols, cvImage2->rows, cvImage2->step, QImage::Format_RGB888)));
@@ -83,6 +202,7 @@ void MainWindow::changeImage(){
   else{
     this->label1->setPixmap(QPixmap::fromImage(QImage(cvImage->data, cvImage->cols, cvImage->rows, cvImage->step, QImage::Format_RGB888)));
   }
+  */
 
 
 }
--- workspace/src/interface/src/interface/mainwindow.h
+++ workspace/src/interface/src/interface/mainwindow.h
@@ -13,9 +13,37 @@
 #include "opencv2/core.hpp"
 #include <opencv/cv.hpp>
 
+#include <traitement/annotation.h>
+
+
+#include <hl_communication/utils.h>
+
+#include <hl_communication/message_manager.h>
+#include <hl_monitoring/monitoring_manager.h>
+#include <hl_monitoring/utils.h>
+#include <hl_monitoring/field.h>
+#include <traitement/position.h>
+#include <traitement/direction.h>
+#include <traitement/team.h>
+
+
+
+#include <opencv2/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <tclap/CmdLine.h>
+#include <string>
+#include <fstream>
+
+#include <sstream>
+
 
 
 using namespace cv;
+using namespace hl_communication;
+using namespace hl_monitoring;
+
+using namespace traitement;
 
 
 class MainWindow : public QMainWindow
@@ -29,6 +57,14 @@ class MainWindow : public QMainWindow
         cv::Mat * cvImage2;
 
         QLabel * label1;
+
+        MonitoringManager manager;
+        uint64_t now;
+        uint64_t dt;
+        Field field;
+        Annotation *annotation;
+        std::map<int, Team> teams;
+
 
         int tour;
 
