f7dbd3b20484bef812b186e1f834e3ca133a87cb
Lucie MATHE <lucie.mathe@etu.u-bordeaux.fr>
2019-04-03 00:17:11 +0200

    mise à jour fichiers de test - en cours

--- workspace/src/traitement/tools/test_time.cpp
+++ workspace/src/traitement/tools/test_time.cpp
@@ -51,7 +51,7 @@ int main(int argc, char ** argv) {
     exit(EXIT_FAILURE);
   }
   
-  fprintf(csv, "Time getCalibratedImage; Time cloneCalibratedImage; Time Annotations Field&Score;Time addAnnotations; total elapsed_time; Time for next image\n");
+  fprintf(csv, "Time to update messages;Time getCalibratedImage;Time cloneCalibratedImage;Time Annotations Field&Score;Time addAnnotations;Total elapsed_time;Action for next image\n");
   
   std::ifstream match_settings("match_settings.json");
   if (!match_settings.good())
@@ -59,6 +59,10 @@ int main(int argc, char ** argv) {
   match_settings >> root;
 
   checkMember(root["match_setting"], "config");
+  checkMember(root["match_setting"], "speed_optimized");
+  bool speed_optimized = root["match_setting"]["speed_optimized"].asBool();
+  
+  
   std::string conf = root["match_setting"]["config"].asString();
   std::cout << conf << std::endl;
 
@@ -70,7 +74,8 @@ int main(int argc, char ** argv) {
   Annotation annotation("annotation_settings.json");
   std::map<int, Team>teams;
   uint64_t now = 0;
-  uint64_t begin_time = now;
+  uint64_t begin_time = 0;
+  uint64_t end_video = 0;
 
   uint64_t time_stamp = 0; //used for calculs
 
@@ -82,12 +87,22 @@ int main(int argc, char ** argv) {
   std::chrono::duration<double, std::micro> elapsed_useconds = end-start;
 
   if (!manager.isLive()) {
-    now = manager.getStart();
+    std::set<std::string> image_provider =  manager.getImageProvidersNames();
+    
+    for (const auto & entry : image_provider)
+      {
+	
+	if (now < manager.getImageProvider(entry).getStart())
+	  now =  manager.getImageProvider(entry).getStart();
+	if (end_video == 0 || end_video <  manager.getImageProvider(entry).getEnd())
+	  end_video =  manager.getImageProvider(entry).getEnd();
+      }
+    
     begin_time = now;
   }
 
 
-  while(manager.isGood()) {
+  while((now-begin_time) / NEXT_FRAME< 100) {    
     manager.update();
     if (manager.isLive()) {
       now = getTimeStamp();
@@ -100,19 +115,40 @@ int main(int argc, char ** argv) {
     
     MessageManager::Status status = manager.getStatus(now);
 
-    for (int idx = 0; idx < status.gc_message.teams_size(); idx++)
+     for (int idx = 0; idx < status.gc_message.teams_size(); idx++)
       {
 	const GCTeamMsg& team_msg = status.gc_message.teams(idx);
 	if (team_msg.has_team_number() && team_msg.has_score())
 	  {
 	    uint32_t team_number = team_msg.team_number();
-	    if (teams.find(team_number)==teams.end()){
-	      Team t1;
-	      teams[team_number]=t1;
-	    }
-	    teams[team_number].setScore(team_msg.score());
+	    if (teams.find(team_number)!=teams.end())
+	      teams[team_number].setScore(team_msg.score());
 	  }
       }
+    
+    for (const auto & robot_entry : status.robot_messages) 
+      {
+	uint32_t team_id = robot_entry.first.team_id();
+	if (teams.find(team_id)==teams.end()){
+	  Team t1;
+	  teams[team_id]=t1;
+	}
+	if (!teams[team_id].IsRobot(robot_entry.first.robot_id())){
+	  teams[team_id].AddRobot(robot_entry.first.robot_id());
+	  teams[team_id].setRobotTeam(robot_entry.first.robot_id(),team_id);
+	  teams[team_id].setRobotNum(robot_entry.first.robot_id());
+	}
+
+	if (robot_entry.second.time_stamp() !=  teams[team_id].getRobot(robot_entry.first.robot_id()).getMessageRobot().time_stamp())
+	  teams[team_id].updateRobot(robot_entry.first.robot_id(), robot_entry.second);
+      }
+
+     
+    end = std::chrono::system_clock::now();
+    elapsed_useconds =end-start;
+    time_stamp = elapsed_useconds.count();
+    fprintf(csv,"%" PRIu64 ";", time_stamp);
+    start = std::chrono::system_clock::now();
     
     std::map<std::string, CalibratedImage> images_by_source =
       manager.getCalibratedImages(now);
@@ -124,8 +160,10 @@ int main(int argc, char ** argv) {
     start = std::chrono::system_clock::now();
     
     for (const auto & entry : images_by_source) {
-      elapsed_useconds =end-begin;
-      if ((now-begin_time-elapsed_useconds.count())>=-3000)
+      end = std::chrono::system_clock::now();
+      elapsed_useconds= end-begin;
+      
+       if (elapsed_useconds.count()<=(now-begin_time) || !speed_optimized)
 	{
 	  cv::Mat display_img = entry.second.getImg().clone();
 
@@ -150,20 +188,7 @@ int main(int argc, char ** argv) {
 	    start = std::chrono::system_clock::now();
 	  
 	    for (const auto & robot_entry : status.robot_messages) {
-	      uint32_t team_id = robot_entry.first.team_id();
-	      if (teams.find(team_id)==teams.end()){
-		Team t1;
-		teams[team_id]=t1;
-	      }
-	      if (!teams[team_id].IsRobot(robot_entry.first.robot_id())){
-		teams[team_id].AddRobot(robot_entry.first.robot_id());
-		teams[team_id].setRobotTeam(robot_entry.first.robot_id(),team_id);
-		teams[team_id].setRobotNum(robot_entry.first.robot_id());
-	      }
-	      if (robot_entry.second.time_stamp() !=  teams[team_id].getRobot(robot_entry.first.robot_id()).getMessageRobot().time_stamp())
-	      teams[team_id].updateRobot(robot_entry.first.robot_id(), robot_entry.second);
-
-	      display_img =annotation.AddAnnotation(camera_information, teams[team_id].getRobot(robot_entry.first.robot_id()) , display_img, now);
+	      display_img =annotation.AddAnnotation(camera_information, teams[robot_entry.first.team_id()].getRobot(robot_entry.first.robot_id()) , display_img, now);
 
 	    }
 
@@ -177,36 +202,46 @@ int main(int argc, char ** argv) {
 	
 	
 
-	  cv::namedWindow(entry.first, cv::WINDOW_AUTOSIZE);
+	  /*	  cv::namedWindow(entry.first, cv::WINDOW_AUTOSIZE);
 	  cv::imshow(entry.first, display_img);
 
 
 	  end = std::chrono::system_clock::now();
 	  elapsed_useconds = end-loop;
-	  time_stamp = elapsed_useconds.count();
+	  time_stamp = elapsed_useconds.count();a
 	  fprintf(csv,"%" PRIu64 ";", time_stamp);
-	  elapsed_useconds = end-begin;
+	  elapsed_useconds = end-begin;*/
+
+	  
 	  	  
+	  if(!speed_optimized)
+	    {
+	      fprintf(csv, "non_optimized \n");
+	      cv::waitKey(1);
+	    }
 	  
-	  if (elapsed_useconds.count() >=(now-begin_time)-SECONDS_TO_MS){
-	    cv::waitKey(1);
-	    fprintf(csv, "fast\n");  
-	  }
-	  else{
-	    cv::waitKey(((now-begin_time)-elapsed_useconds.count())/SECONDS_TO_MS);
-	    fprintf(csv, "slow\n");
+	  else
+	    {
+	      end = std::chrono::system_clock::now();	      
+	      elapsed_useconds = end-begin;
+	      if (elapsed_useconds.count() >=(now-begin_time)-SECONDS_TO_MS){
+		cv::waitKey(1);
+		fprintf(csv, "fast \n");
+	      }
+	      else{
+		cv::waitKey(((now-begin_time)-elapsed_useconds.count())/SECONDS_TO_MS);
+		fprintf(csv, "slow \n");
+	      }
+	    }
+	 
 	  
-	  }
-	
 	}
+       else
+	 {
+	   fprintf(csv, " skip image \n");
+	 }
        
-	
-      else{
-	elapsed_useconds = end-loop;
-	time_stamp = elapsed_useconds.count();
-	fprintf(csv, "0;0;0;%" PRIu64 ";skip image\n", time_stamp);
-	  
-      }
+       
     }  
   }
   return 0;
